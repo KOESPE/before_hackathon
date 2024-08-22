987f4b8fba3b178dabf9a6bbe26f9d4e6ad3a74c
koespe <kirillsportige@gmail.com>
2024-08-22 18:46:05 +0300

    added CORS middleware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.auth import auth_router
 from config import settings
@@ -11,6 +12,13 @@ async def lifespan():
 
 
 app = FastAPI(on_startup=[lifespan])
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=["*"],
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"],
+                   )
 
 @app.get('/')
 async def get_db_data():
